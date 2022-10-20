cbb12769dec4cdcb1aeef7e5e261746151486743
Victor Sago <VictorSago01@gmail.com>
2022-10-20 14:43:27 +0200

    Post request

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/add-product', (req, res, next) => {
   res.send(rstring);
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
