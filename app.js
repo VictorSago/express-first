3300820fd68e54a864c6021ad9b296e5e3985775
Victor Sago <VictorSago01@gmail.com>
2022-10-20 18:35:04 +0200

    Adding a 404 page

--- app.js
+++ app.js
@@ -17,5 +17,8 @@ app.use('/', (req, res, next) => {
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
