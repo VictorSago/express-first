1889fda2edf41e03db461067613811a8a2f6dce3
Victor Sago <VictorSago01@gmail.com>
2022-10-20 15:09:00 +0200

    Express router

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', (req, res, next) => {
@@ -11,24 +14,8 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.use('/add-product', (req, res, next) => {
-  //console.log("In another middleware");
-  let rstring = "<html><head><title>Add Product</title></head>";
-  rstring += "<body>";
-  rstring += '<h1>The "Add Product" page</h1>';
-  rstring += '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>';
-  rstring += "</body></html>";
-  res.send(rstring);
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  //console.log("In root middleware");
-  res.send('<h1>Hellllllo from Exxxxpressssss!</h1>');
-});
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  //console.log("In another middleware");
+  let rstring = "<html><head><title>Add Product</title></head>";
+  rstring += "<body>";
+  rstring += '<h1>The "Add Product" page</h1>';
+  rstring +=
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>';
+  rstring += "</body></html>";
+  res.send(rstring);
+});
+
+router.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+
+module.exports = router;
