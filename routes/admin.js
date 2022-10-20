0ab7e84d765bfb8250ca29a8f33f3144dfa11d46
Victor Sago <VictorSago01@gmail.com>
2022-10-20 19:55:47 +0200

    Added helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require("express");
 
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
@@ -12,7 +14,7 @@ router.get("/add-product", (req, res, next) => {
   rstring +=
     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>';
   rstring += "</body></html>"; */
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "..", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const path = require("path");
 const express = require("express");
 
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   //console.log("In root middleware");
   //res.send("<h1>Hellllllo from Exxxxpressssss!</h1>");
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "..", "views", "shop.html"));
 });
 
 
