b8912b13f4fc965ef4e6c1229d0f90f7e7c24f40
Victor Sago <victor.sago@elstandard.se>
2022-10-24 11:20:01 +0200

    Serving files statically

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(express.static(path.join(rootDir, "public")));
+
 app.use('/', (req, res, next) => {
   //console.log("This middleware always runs!");
   next();
