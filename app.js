81b8582750aadecef1f2b71da93452ea132e12b9
Victor Sago <victor.sago@elstandard.se>
2022-11-22 12:00:01 +0100

    Added layout

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   //res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
-  res.status(404).render("404");
+  res.status(404).render("404", {pageTitle: "Pag e Not Found"});
 });
 
 app.listen(3000);
